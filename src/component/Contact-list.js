eb2596881619bfa54749ebe1e20fbfa6ccf746bb
Youssouf <youssouf.adenomar@gmail.com>
2020-10-15 14:06:59 +0200

    contact-list

---
+++ src/component/Contact-list.js
@@ -0,0 +1,43 @@
+import React from "react";
+import Contact from "./Contact";
+
+const users = [
+  {
+    name: 'Robert Reyes',
+    avatar: 'https://randomuser.me/api/portraits/men/28.jpg',
+    online: false
+  },
+  {
+    name: 'Nellie Caldwell',
+    avatar: 'https://randomuser.me/api/portraits/women/17.jpg',
+    online: true
+  },
+  {
+    name: 'Vernon Mason',
+    avatar: 'https://randomuser.me/api/portraits/men/84.jpg',
+    online: true
+  },
+  {
+    name: 'Erica Hunt',
+    avatar: 'https://randomuser.me/api/portraits/men/84.jpg',
+    online: false
+  },
+  {
+    name: 'Juanita Phillips',
+    avatar: 'https://randomuser.me/api/portraits/women/55.jpg',
+    online: true
+  }
+];
+const contactlist = () => (
+  <div>
+    {users.map((item) => (
+  < Contact 
+  name ={item.name} 
+  avatar={item.avatar} 
+  online={item.online}
+  />
+    ))}
+  </div>
+);
+
+export default contactlist;
